e8768cb66c14c1b17a4e544fc97defbab84cda17
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-12-17 13:42:46 +0100

    Initial commit

--- svenska/settings.py
+++ svenska/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o_(@uoem2n4f^w@_*rxisgx%t-56m_xc$j8o*cr(_4x2^lu%+@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '209.38.202.27']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '209.38.202.27', 'rattatext.se', 'www.rattatext.se']
 
 
 
@@ -131,3 +131,6 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
